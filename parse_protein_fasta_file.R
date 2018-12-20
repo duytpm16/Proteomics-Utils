c22a17a092f37558ff487f7b25455994a729a87e
duytpm16 <39827376+duytpm16@users.noreply.github.com>
2018-12-19 21:27:04 -0500

    Update parse_protein_fasta_file.R

--- parse_protein_fasta_file.R
+++ parse_protein_fasta_file.R
@@ -1,28 +1,78 @@
-n <- length(k)
+options(stringsAsFactors = FALSE)
+library(seqinr)
 
 
-protein_df <- data.frame(protein_id   = character(length = n),
-                         chromosome   = character(length = n),
-                         start        = numeric(length = n),
-                         end          = numeric(length = n),
-                         gene_id      = character(length = n),
-                         trascript_id = character(length = n),
-                         gene_symbol  = character(length = n))
+### Variables to change
+#   1.) fasta.file  : path to protein fasta file
+#   2.) mb          : logical value. True - convert numeric positions to Mb  | False - leave positions as is
+#   3.) outfile.name: file name to save results  
+fasta.file   <- 'Ensembl 90 Proteins/Mus_musculus.GRCm38.pep.all.fa'
+mb           <- TRUE
+outfile.name <- 'reference_protein_info.rds' 
 
 
 
 
+
+
+
+
+
+### Create data frame to store protein header info
+fasta.file <- read.fasta(fasta.file)
+
+
+n <- length(fasta.file)
+fasta_file_df <- data.frame(protein_id         = character(length = n),
+                            chromosome         = character(length = n),
+                            start              = numeric(length = n),
+                            end                = numeric(length = n),
+                            gene_id            = character(length = n),
+                            transcript_id      = character(length = n),
+                            gene_biotype       = character(length = n),
+                            transcript_biotype = character(length = n),
+                            gene_symbol        = character(length = n),
+                            description        = character(length = n))
+
+
+
+
+
+
+
+
+### Parsing fasta file begins
 for(i in 1:n){
     
-    info <- strsplit(getAnnot.SeqFastaAA(k[[i]]), split = ' ')[[1]]                      # Get fasta header of protein sequence
-    protein_df$protein_id[i]    <- gsub('>','',info[1])                                  # Get protein id
-    protein_df$chromosome[i]    <- strsplit(info[3], split = ':')[[1]][3]                # Get chromosome
-    protein_df$start[i]         <- as.numeric(strsplit(info[3], split = ':')[[1]][4])    # Get start
-    protein_df$end[i]           <- as.numeric(strsplit(info[3], split = ':')[[1]][5])    # Get end
-    protein_df$gene_id[i]       <- strsplit(info[4], split = ':')[[1]][2]                # Get gene id
-    protein_df$trascript_id[i]  <- strsplit(info[5], split = ':')[[1]][2]                # Get transcript id
-    protein_df$gene_symbol[i]   <- strsplit(info[8], split = ':')[[1]][2]                # Get gene_symbol
+    # Get header description for a protein
+    info <- strsplit(getAnnot(fasta.file[[i]]), split = ' ')[[1]]
+  
+  
+  
+    # Add the info to fasta_file_df
+    fasta_file_df$protein_id[i]         <- gsub('>','',info[1])
+    fasta_file_df$chromosome[i]         <- strsplit(info[3], split = ':')[[1]][3]
+    fasta_file_df$start[i]              <- strsplit(info[3], split = ':')[[1]][4]
+    fasta_file_df$end[i]                <- strsplit(info[3], split = ':')[[1]][5]
+    fasta_file_df$gene_id[i]            <- strsplit(info[4], split = ':')[[1]][2]
+    fasta_file_df$transcript_id[i]      <- strsplit(info[5], split = ':')[[1]][2]
+    fasta_file_df$gene_biotype[i]       <- strsplit(info[6], split = ':')[[1]][2]
+    fasta_file_df$transcript_biotype[i] <- strsplit(info[7], split = ':')[[1]][2]
+    fasta_file_df$gene_symbol[i]        <- strsplit(info[8], split = ':')[[1]][2]
+    fasta_file_df$description[i]        <- strsplit(info[9], split = ':')[[1]][2]
   
 }
 
 
+
+### Convert positions to Mb if mb == TRUE
+if(mb){
+   fasta_file_df$start <- fasta_file_df$start / 1e6
+   fasta_file_df$end   <- fasta_file_df$end / 1e6
+}
+
+
+
+
+
+saveRDS(fasta_file_df, file = out.file)
